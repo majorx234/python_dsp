7422c26d0d6594efb4cd2bbc916358ec305289c0
Majorx234 <majorx234@googlemail.com>
2023-05-30 23:58:13 +0200

    Add function to create magnitude from filter coefficients

--- filter_design/fir_filter_design.py
+++ filter_design/fir_filter_design.py
@@ -16,6 +16,21 @@ def create_fir_from_magnitude(magnitude_samples):
     impulse_response = np.fft.ifft(response)
     return (impulse_response, magnitude_response, phase_response)
 
+def create_magnitude_from_filter_coeffs(filter_coeffs,s):
+    (num_coeffs, den_coeffs) = filter_coeffs
+    num_coeffs_len = num_coeffs.size
+    den_coeffs_len = den_coeffs.size
+
+    num = 0;
+    for i in range(0, num_coeffs_len):
+        num += num_coeffs[i]* np.power(s, num_coeffs_len - i)
+
+    den = 0;
+    for i in range(0, den_coeffs_len):
+        den += den_coeffs[i]* np.power(s, den_coeffs_len - i)
+
+    return num/den
+
 
 def main():
     magnitude_samples = np.ones(64)
