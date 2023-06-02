f706f9a77a3656610ebed5321fe707b54b9a6be4
Majorx234 <majorx234@googlemail.com>
2023-06-02 23:56:47 +0200

    Add function for create white noise and to limit signal

--- signal_processing/wave.py
+++ signal_processing/wave.py
@@ -14,3 +14,25 @@ class Wave:
         for x in range(self.num_samples):
             sine_wave[x] = np.sin(2 * np.pi * self.frequency * x/self.sampling_rate)
         return sine_wave
+
+    def create_fm_sine(self, freq_fm) -> np.array:
+        fm_sine = np.zeros((self.num_samples,), dtype=float)
+
+def limit_signal(signal: np.array, max: float = 1.0, min: float = -1.0) -> np.array:
+    limit_signal = np.zeros((signal.size,), dtype=float)
+    for idx in range(0,signal.size):
+        sample = signal[idx]
+        if sample > max:
+            sample = max
+        if sample < min:
+            sample = min
+        limit_signal[idx] = sample
+    return limit_signal
+
+def generate_white_noise(nsamples: int) -> np.array:
+    mean = 0.0
+    scale = 1.0
+    noise_signal = np.zeros((nsamples,), dtype=float)
+    for idx in range(0,nsamples):
+        noise_signal[idx] = np.random.normal(mean,scale)
+    return noise_signal
